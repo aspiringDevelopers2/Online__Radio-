f70e5b55df83997efd38be671242696fa0590f5a
Ibukunoluwa <oyetunjiibukunoluwa8@gmail.com>
2023-05-13 14:59:57 +0100

    updating the link Model

--- models/linkModel.js
+++ models/linkModel.js
@@ -1,2 +1,9 @@
 const mongoose = require("mongoose");
-const linkSchema = new mongoose.Schema();
+const linkSchema = new mongoose.Schema({
+  link: { type: String },
+  Name: { type: String },
+  Description: {
+    type: String,
+  },
+});
+module.exports = mongoose.model("Link", linkSchema);
